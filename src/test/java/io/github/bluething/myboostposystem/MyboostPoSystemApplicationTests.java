4e8c1e3993014cc026a0f4be2582f4b88984f1f3
Habib Ali Machpud <habib.machpud@gmail.com>
2025-09-24 17:33:31 +0700

    Init

---
+++ src/test/java/io/github/bluething/myboostposystem/MyboostPoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.myboostposystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboostPoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
